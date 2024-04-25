68f0ba835a1b60570832df5003cbb2fa3b39a658
Antrikas <andreas.antoniades17@gmail.com>
2024-04-25 13:04:50 +0300

    maxChar

--- questions/maxChar/index.js
+++ questions/maxChar/index.js
@@ -7,7 +7,24 @@
  */
 
 const maxCharacter = (str) => {
+    const charMap = new Map();
+    let max = 0;
+    let maxChar = "";
+    
   
-};
+    for (const char of str) {
+        charMap.set(char, charMap.get(char) + 1 || 1);
+    }
+
+    for ( const [char,  count]  of charMap) {
+      if (count > max) {
+        max = count;
+        maxChar = char;
+      }
+       }
+       return maxChar;
+    };
+   
+
 
 module.exports = maxCharacter;
